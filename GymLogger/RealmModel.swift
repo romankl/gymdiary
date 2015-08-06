953e9726cd1e7adf4f1c3f8aad0b1b3bd5a2186b
Roman Klauke <romaaan.git@gmail.com>
2015-08-06 20:05:43 +0200

    extended model with new table and removed unused columns

--- GymLogger/RealmModel.swift
+++ GymLogger/RealmModel.swift
@@ -11,6 +11,9 @@ import RealmSwift
 
 
 class Exercise: Object {
+    dynamic var createdAt = NSDate() // when was it created?
+    dynamic var updatedAt = NSDate()
+
     dynamic var name = "" // workout name
     dynamic var type = 0  // workout type (0 = weight, 1 = time/ distance)
     dynamic var builtin = false // is it a built in Exercise
@@ -19,8 +22,6 @@ class Exercise: Object {
     dynamic var lastUsed = NSDate() // when was this exercise the last time
     dynamic var used = 0 // inc each time this one is used
     dynamic var archived = false // built in exercises can be archived
-    dynamic var createdAt = NSDate() // when was it created?
-    dynamic var updatedAt = NSDate()
 
     override class func primaryKey() -> String {
         return "name"
@@ -42,26 +43,31 @@ class Exercise: Object {
 
 /// A mapping object that maps the weight/ distance or time to each exercise
 class PerformanceExerciseMap: Object {
-    dynamic var exercise: Exercise!
-    let detailPerformance = List<Performance>()
     dynamic var createdAt = NSDate() // when was it created?
     dynamic var updatedAt = NSDate()
+
+    dynamic var exercise: Exercise!
+    // all the performed sets for this exercise are store using the list.
+    // An object can be used for weights or distances depending on the used exercise.
+    let detailPerformance = List<Performance>()
 }
 
+/// Each set is available as a seperate object
 class Performance: Object {
     dynamic var createdAt = NSDate() // when was it created?
     dynamic var updatedAt = NSDate()
+
     dynamic var weight = Double()
     dynamic var reps = 0
     dynamic var distance = Double()
     dynamic var time =  Double()
-    dynamic var type = 0
 }
 
 /// Performed Workouts
 class Workout: Object {
     dynamic var createdAt = NSDate() // when was it created?
     dynamic var updatedAt = NSDate()
+
     let performedExercises = List<PerformanceExerciseMap>() // "To many" for the
     dynamic var name = "" // name of the workout -> should be the copy of the original workout
     dynamic var startedAt = NSDate() // date when the workout was pe
@@ -74,6 +80,8 @@ class Workout: Object {
     dynamic var totalWeight = Double()
     dynamic var totalDistance = Double()
     dynamic var totalRunningTime = Double() // Defines the total time of all running exercises - not the total time of the workout
+
+    dynamic var progressPic = ""
 
     // grouping property
     var performedInWeekOfYear: Int {
@@ -90,6 +98,7 @@ class Workout: Object {
 class WorkoutRoutine: Object {
     dynamic var createdAt = NSDate() // when was it created?
     dynamic var updatedAt = NSDate()
+
     dynamic var name = ""
     dynamic var comment = ""
     dynamic var color = "" // Workout "color" code
@@ -110,4 +119,29 @@ class WorkoutRoutine: Object {
     var performedWorkouts: [Workout] {
         return linkingObjects(Workout.self, forProperty: "basedOnWorkout")
     }
+}
+
+/// Values that are calculated through the whole model.
+/// Stored here to reduce the total calculation time
+class Summary: Object {
+    // TODO: Not so sure, if this one could be better solved using a query
+    // to avoid doubled data.
+    // The idea behind it is, that i could store all the calculated values
+    // after the workout on this one object and use them for different statistics
+    dynamic var createdAt = NSDate() // when was it created?
+    dynamic var updatedAt = NSDate()
+
+    dynamic var totalSets = 0
+    dynamic var totalReps = 0
+    dynamic var totalDistance = Double()
+    dynamic var totalTime = NSDate()
+    dynamic var totalWeight = Double()
+    dynamic var totalWorkouts = 0
+    dynamic var totalUsedExercises = 0
+    dynamic var totalAddedExercises = 0
+    dynamic var totalTrackedTime = NSDate()
+    dynamic var avgWorkoutDuration = NSDate()
+    dynamic var avgRepsPerWorkout = 0
+    dynamic var avgSetsPerWorkout = 0
+    dynamic var avgWeightPerRep = Double()
 }
