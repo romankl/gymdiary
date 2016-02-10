89f8121f996c2a70ee3603e2635ba43cc4bb7eda
Roman Klauke <romaaan.git@gmail.com>
2016-02-10 21:19:05 +0100

    implement a way to store the used barbell for each exercise

---
+++ GymLogger/entities/BarbellTypes.swift
@@ -0,0 +1,23 @@
+//
+//  BarbellTypes.swift
+//  GymLogger
+//
+//  Created by Roman Klauke on 10.02.16.
+//  Copyright © 2016 Roman Klauke. All rights reserved.
+//
+
+import Foundation
+
+
+enum BarbellTypes: Int, CustomStringConvertible {
+    case Olympic = 0, Ez, Triceps, Shrug
+
+    public var description: String {
+        switch self {
+        case .Olympic: return NSLocalizedString("Olympic Barbell", comment: "Barbell Type")
+        case .Ez: return NSLocalizedString("EZ Curl Bar", comment: "EZ Bar")
+        case .Triceps: return NSLocalizedString("Triceps Bar", comment: "Triceps Bar")
+        case .Shrug: return NSLocalizedString("Shrug/ Trap Bar", comment: "Shrug Bar")
+        }
+    }
+}
--- GymLogger/entities/PerformanceExerciseMapEntity+CoreDataProperties.swift
+++ GymLogger/entities/PerformanceExerciseMapEntity+CoreDataProperties.swift
@@ -2,8 +2,8 @@
 //  PerformanceExerciseMapEntity+CoreDataProperties.swift
 //  GymLogger
 //
-//  Created by Roman Klauke on 31.12.15.
-//  Copyright © 2015 Roman Klauke. All rights reserved.
+//  Created by Roman Klauke on 10.02.16.
+//  Copyright © 2016 Roman Klauke. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -17,8 +17,9 @@ extension PerformanceExerciseMapEntity {
     @NSManaged var completedSets: NSNumber?
     @NSManaged var isComplete: NSNumber?
     @NSManaged var plannedSets: NSNumber?
-    @NSManaged var volatileId: String?
     @NSManaged var totalReps: NSNumber?
+    @NSManaged var volatileId: String?
+    @NSManaged var usedBarbell: NSNumber?
     @NSManaged var exercise: ExerciseEntity?
     @NSManaged var performance: NSOrderedSet?
     @NSManaged var workout: WorkoutEntity?
