43200966822d0cb276424d66ba56381e3fb78cc7
Roman Klauke <romaaan.git@gmail.com>
2016-03-22 20:27:49 +0100

    add the scope entity

---
+++ GymLogger/entities/BodyPartScopeEntity.swift
@@ -0,0 +1,17 @@
+//
+//  BodyPartScopeEntity.swift
+//  GymLogger
+//
+//  Created by Roman Klauke on 22.03.16.
+//  Copyright © 2016 Roman Klauke. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class BodyPartScopeEntity: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
